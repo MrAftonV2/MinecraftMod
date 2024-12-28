6cbfe272c6449f2f54a386aabab511f761b5936d
MrAftonV2 <133411516+MrAftonV2@users.noreply.github.com>
2024-12-29 01:42:06 +0200

    nimic

---
+++ src/main/java/items/ModItems.java
@@ -0,0 +1,17 @@
+package items;
+
+import net.horia.tutorial.TurorialMod;
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> Items =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TurorialMod.MOD_ID);
+
+
+    public static void register() {
+
+    }
+}
